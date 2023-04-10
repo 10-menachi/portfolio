7f3b341175c0744547c9053216583036a10c611f
chrisdroid <bobitlmr188221@spu.ac.ke>
2023-04-10 18:04:06 +0300

    Add popup content

--- index.js
+++ index.js
@@ -3,6 +3,45 @@ const mobileMenu = document.querySelector('.mobile-menu');
 const closeMenu = document.querySelector('.close-icon');
 const mobileLinks = document.querySelectorAll('.mobile-link');
 
+const popup_details = [
+  {
+    name: 'Tonic',
+    description:
+      'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    image: 'assets/snapshot-1.png',
+    technologies: ['html', 'css', 'javascript'],
+    linkToLive: '',
+    linkToSource: '',
+  },
+  {
+    name: 'Multi-Post Stories',
+    description:
+      'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    image: 'assets/snapshot-2.png',
+    technologies: ['html', 'css', 'javascript'],
+    linkToLive: '',
+    linkToSource: '',
+  },
+  {
+    name: 'Tonic',
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+    image: 'assets/snapshot-3.png',
+    technologies: ['html', 'css', 'javascript'],
+    linkToLive: '',
+    linkToSource: '',
+  },
+  {
+    name: 'Tonic',
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+    image: 'assets/snapshot-4.png',
+    technologies: ['html', 'css', 'javascript'],
+    linkToLive: '',
+    linkToSource: '',
+  },
+];
+
 hamMenu.addEventListener('click', () => {
   mobileMenu.classList.toggle('active');
 });
