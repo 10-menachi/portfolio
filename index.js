d4f18ccdc7208c66324fcedac91f29a7c28aca16
Junaid Ahmed Syed <61676854+iamsjunaid@users.noreply.github.com>
2023-04-11 20:23:07 +0530

    added validation on email

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const mobileMenu = document.querySelector('.mobile-menu');
 const closeMenu = document.querySelector('.close-icon');
 const mobileLinks = document.querySelectorAll('.mobile-link');
 const form = document.querySelector('.contact-form');
+const email = document.querySelector('.email');
 
 hamMenu.addEventListener('click', () => {
   mobileMenu.classList.toggle('active');
@@ -22,5 +23,11 @@ form.addEventListener('submit', (e) => {
   if (email.value !== email.value.toLowerCase()) {
     email.setCustomValidity('Email must be in lowercase');
     e.preventDefault();
+  }
+});
+
+email.addEventListener('input', () => {
+  if (email.validity) {
+    email.setCustomValidity('');
   }
 });
